579693054e2745e0227291b48f87435085dfb6b3
rendick <rendick123@proton.me>
2024-05-13 21:56:34 +0300

    --update flag

--- cmd/args.go
+++ cmd/args.go
@@ -8,11 +8,13 @@ import (
 func Args() {
 	var flagInput string
 	var desyncFlag string
+	var updateFlag string
 	var newsFlag bool
 	var configFlag bool
 	var testFlag bool
 	flag.StringVar(&flagInput, "sync", "", "sync the package with your system")
 	flag.StringVar(&desyncFlag, "desync", "", "desync the package with ")
+	flag.StringVar(&updateFlag, "update", "", "update the package or system")
 	flag.BoolVar(&newsFlag, "news", false, "display operating system news")
 	flag.BoolVar(&configFlag, "config", false, "configurate package manager")
 	flag.BoolVar(&testFlag, "test", false, "test")
@@ -25,6 +27,14 @@ func Args() {
 
 	if desyncFlag != "" {
 		Desync(desyncFlag)
+		return
+	}
+
+	if updateFlag == "@world" {
+		UpdateSystem()
+		return
+	} else if updateFlag != "" {
+		UpdatePackage(updateFlag)
 		return
 	}
 
---
+++ cmd/update.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+)
+
+func UpdateSystem() {
+	fmt.Println("world")
+}
+
+func UpdatePackage(pkg string) {
+	jsonPath := "/home/rendick/.config/hpm/" + pkg + ".json"
+	fmt.Println(jsonPath)
+	if _, err := os.Stat(jsonPath); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("[1/1] %s directory does not exist\n", jsonPath)
+			return
+		} else {
+			fmt.Printf("Error accessing %s: %v\n", jsonPath, err)
+			return
+		}
+	}
+	fmt.Printf("[1/1] %s directory exists\n", jsonPath)
+	return
+}
