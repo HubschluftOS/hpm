004fbb7dcdbcd19cb37aff491914c57910076921
rendick <rendick123@proton.me>
2024-06-21 23:58:32 +0300

    Added help argument in cli.go \ Edited README

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # Hubshluft Package Manager
 
+Lightweight and powerful package manager for Hubshluft and other GNU/Linux distributions
+
 # Installation
 
-**Soon.**
+Use `make` to install `hpm`.
+
+```bash
+make
+```
 
 # Dependencies
 
-**sudo, sh**
+**sudo, make, sh**
 
-# Documentation
+# Usage
 
-**Soon.**
+```bash
+get	 Install the needed package on your Linux system
+unget	 Uninstall the package on your Linux system
+update	 Update all packages on your Linux system
+news	Display the latest news of the Hubshluft team and HubshluftOS
+help	Display extra information about package manager
+```
 
+# License
+
+[BSD 2-Clause License](https://opensource.org/license/bsd-2-clause)
--- cmd/cli.go
+++ cmd/cli.go
@@ -10,14 +10,29 @@ var (
 	UngetFlag  string
 	NewsFlag   bool
 	UpdateFlag string
+	HelpFlag   bool
 )
 
 func Cli() {
+	flag.BoolVar(&HelpFlag, "help", false, "Display extra information about package manager.")
 	flag.StringVar(&GetFlag, "get", "", "Install the needed package on your Linux system.")
 	flag.StringVar(&UngetFlag, "unget", "", "Uninstall the package on your Linux system.")
 	flag.StringVar(&UpdateFlag, "update", "", "Update all packages on your Linux system.")
 	flag.BoolVar(&NewsFlag, "news", false, "Display the latest news of the Hubshluft team and HubshluftOS.")
 	flag.Parse()
+
+	if HelpFlag {
+		fmt.Printf("Usage: hpm [OPTION]... [PACKAGE]\n" +
+			"Lightweight and powerful package manager for Hubshluft Linux and other GNU/Linux distributions\n\n" +
+			"get\t Install the needed package on your Linux system\n" +
+			"unget\t Uninstall the package on your Linux system\n" +
+			"update\t Update all packages on your Linux system\n" +
+			"news\tDisplay the latest news of the Hubshluft team and HubshluftOS\n" +
+			"help\tDisplay extra information about package manager\n\n" +
+			"Full documentation <https://github.com/hubshluft/hpm>\n" +
+			"Source code <https://github.com/hubshluft/hpm>\n")
+		return
+	}
 
 	if GetFlag != "" {
 		Get(GetFlag)
